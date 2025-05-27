eade65c5f6fa60b6f01bfa3db525633cd33f3d7d
niksisons <rogozhin.nickita2018@yandex.ru>
2025-05-27 18:39:11 +0300

    link2

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ import requests
 import pandas as pd
 import io
 
-BACKEND_URL = "https://backservice-l6s9.onrender.com"
+BACKEND_URL = "https://backservice-l6s9.onrender.com/convert"
 
 st.set_page_config(page_title="🌍 Конвертер координат", layout="centered")
 
